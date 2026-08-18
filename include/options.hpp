731f63ab4246be68c4d5b88e1e78e2fd7ea36b61
Guilherme <leonsombrio244@gmail.com>
2026-08-17 22:40:05 -0300

    Add ConflictMode enum and update Options struct

--- include/options.hpp
+++ include/options.hpp
@@ -5,12 +5,22 @@
 
 namespace organise {
 
+enum class ConflictMode {
+    Interactive,
+    Rename,
+    Skip,
+    Overwrite
+};
+
 struct Options {
     std::filesystem::path targetDir;
     std::filesystem::path configPath;
     bool dryRun = false;
     bool showHelp = false;
     bool showVersion = false;
+    bool recursive = false;
+    bool verbose = false;
+    ConflictMode conflictMode = ConflictMode::Interactive;
 };
 
 Options parseArgs(int argc, char* argv[]);
