db3554ee8914fdf24cc10b5351c1c27b2ea1986b
Guilherme <leonsombrio244@gmail.com>
2026-08-17 22:11:01 -0300

    Update include paths in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "include/config.hpp"
-#include "include/organiser.hpp"
+#include "../include/config.hpp"
+#include "../include/organiser.hpp"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
