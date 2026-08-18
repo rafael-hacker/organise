ebaa6cd28131de629b70c9072a49daf7d2129331
Guilherme <leonsombrio244@gmail.com>
2026-08-18 17:06:22 -0300

    Add color output to config error messages

--- src/config.cpp
+++ src/config.cpp
@@ -1,4 +1,5 @@
 #include "../include/config.hpp"
+#include "../include/colors.hpp"
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -15,7 +16,7 @@ std::filesystem::path getDefaultConfigPath() {
 
 json loadConfig(const std::filesystem::path& path) {
     if (!std::filesystem::exists(path)) {
-        std::cout << "Config file not found. Creating standard config in: " << path << std::endl;
+        std::cout << color::yellow << "Config file not found. Creating standard config in: " << path << color::reset << std::endl;
 
         if (path.has_parent_path()) {
             std::filesystem::create_directories(path.parent_path());
@@ -36,7 +37,7 @@ json loadConfig(const std::filesystem::path& path) {
             outFile << defaultConfig.dump(4) << std::endl;
             outFile.close();
         } else {
-            std::cerr << "\033[31mError: Couldn't create config file in: " << path <<  "\033[0m" << std::endl;
+            std::cerr << color::red << "Error: Couldn't create config file in: " << path <<  color::reset << std::endl;
             return nullptr;
         }
 
@@ -45,7 +46,7 @@ json loadConfig(const std::filesystem::path& path) {
 
     std::ifstream file(path);
     if (!file.is_open()) {
-        std::cerr << "\033[31mError: Couldn't open config file in: " << path << "\033[0m" << std::endl;
+        std::cerr << color::red << "Error: Couldn't open config file in: " << path << color::reset << std::endl;
         return nullptr;
     }
 
@@ -53,7 +54,7 @@ json loadConfig(const std::filesystem::path& path) {
     try {
         file >> data;
     } catch (const json::parse_error& e) {
-        std::cerr << "\033[31mError parsing JSON config: " << e.what() << "\033[0m" <<std::endl;
+        std::cerr << color::red << "Error parsing JSON config: " << e.what() << color::reset << std::endl;
         return nullptr;
     }
 
