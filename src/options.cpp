c30915c2c48f3f1799b6770a4f77ae514d1c6c68
Guilherme <leonsombrio244@gmail.com>
2026-08-17 22:41:08 -0300

    Update help options and argument parsing in options.cpp

--- src/options.cpp
+++ src/options.cpp
@@ -8,10 +8,14 @@ namespace organise {
 void printHelp() {
     std::cout << "Usage: org [OPTIONS] <path>\n\n"
               << "Options:\n"
-              << "  -n, --dry-run     Simulate actions without actually doing them\n"
-              << "  -c, --config PATH Custom configuration file path\n"
-              << "  -h, --help        Display this help message\n"
-              << "  -v, --version     Display version information\n";
+              << "  -n, --dry-run          Simulate actions without moving files\n"
+              << "  -r, --recursive        Scan directories recursively\n"
+              << "  -v, --verbose          Display detailed execution output\n"
+              << "  -y, --auto-rename      Automatically rename conflicting files (no prompt)\n"
+              << "      --conflict <mode>  Set conflict strategy: rename, skip, or overwrite\n"
+              << "  -c, --config <path>    Custom configuration file path\n"
+              << "  -h, --help             Display this help message\n"
+              << "      --version          Display version information\n";
 }
 
 Options parseArgs(int argc, char* argv[]) {
@@ -22,10 +26,21 @@ Options parseArgs(int argc, char* argv[]) {
 
         if (arg == "-h" || arg == "--help") {
             opts.showHelp = true;
-        } else if (arg == "-v" || arg == "--version") {
+        } else if (arg == "--version") {
             opts.showVersion = true;
         } else if (arg == "-n" || arg == "--dry-run") {
             opts.dryRun = true;
+        } else if (arg == "-r" || arg == "--recursive") {
+            opts.recursive = true;
+        } else if (arg == "-v" || arg == "--verbose") {
+            opts.verbose = true;
+        } else if (arg == "-y" || arg == "--auto-rename") {
+            opts.conflictMode = ConflictMode::Rename;
+        } else if (arg == "--conflict" && i + 1 < argc) {
+            std::string_view mode = argv[++i];
+            if (mode == "skip") opts.conflictMode = ConflictMode::Skip;
+            else if (mode == "overwrite") opts.conflictMode = ConflictMode::Overwrite;
+            else opts.conflictMode = ConflictMode::Rename;
         } else if ((arg == "-c" || arg == "--config") && i + 1 < argc) {
             opts.configPath = argv[++i];
         } else if (arg[0] != '-') {
