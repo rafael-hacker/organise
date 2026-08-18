5bb8cd048739b786ae44952797bf4e2bf71bb4e5
Guilherme <leonsombrio244@gmail.com>
2026-08-17 21:46:01 -0300

    Add config handling functions in config.cpp
    Implement functions to get default config path and load config from file.

---
+++ src/config.cpp
@@ -0,0 +1,28 @@
+#include "config.hpp"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+namespace organise {
+
+std::filesystem::path getDefaultConfigPath() {
+    const char* homeDir = std::getenv("HOME");
+    if (!homeDir) {
+        return {};
+    }
+    return std::filesystem::path(homeDir) / ".config" / "organise" / "config.json";
+}
+
+json loadConfig(const std::filesystem::path& path) {
+    std::ifstream file(path);
+    if (!file.is_open()) {
+        std::cerr << "Error: couldn't open config file in " << path << std::endl;
+        return nullptr;
+    }
+
+    json data;
+    file >> data;
+    return data;
+}
+
+} // namespace organise
