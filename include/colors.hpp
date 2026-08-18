9245fe3ffa512e8e60e0b53466e96c4678b56738
rafael <allain.123ant@gmail.com>
2026-08-18 18:52:46 -0300

    Enhance ux

--- include/colors.hpp
+++ include/colors.hpp
@@ -3,7 +3,8 @@
 #include <string_view>
 
 namespace organise::color {
-    // Códigos ANSI de cor
+    // ANSI color codes
+    constexpr std::string_view purple = "\033[35m";
     constexpr std::string_view reset  = "\033[0m";
     constexpr std::string_view red    = "\033[31m";
     constexpr std::string_view green  = "\033[32m";
