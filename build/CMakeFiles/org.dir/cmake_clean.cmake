8a141a27aca54abac1a7c7c711cd78944b3eaf92
rafael <allain.123ant@gmail.com>
2026-08-18 20:40:28 -0300

    refatoring argument parser with getopt

---
+++ build/CMakeFiles/org.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/org.dir/link.d"
+  "CMakeFiles/org.dir/src/config.cpp.o"
+  "CMakeFiles/org.dir/src/config.cpp.o.d"
+  "CMakeFiles/org.dir/src/history.cpp.o"
+  "CMakeFiles/org.dir/src/history.cpp.o.d"
+  "CMakeFiles/org.dir/src/main.cpp.o"
+  "CMakeFiles/org.dir/src/main.cpp.o.d"
+  "CMakeFiles/org.dir/src/options.cpp.o"
+  "CMakeFiles/org.dir/src/options.cpp.o.d"
+  "CMakeFiles/org.dir/src/organiser.cpp.o"
+  "CMakeFiles/org.dir/src/organiser.cpp.o.d"
+  "CMakeFiles/org.dir/src/watcher.cpp.o"
+  "CMakeFiles/org.dir/src/watcher.cpp.o.d"
+  "org"
+  "org.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/org.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
